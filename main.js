e9c5640983f6b008d7ee8a59c3450009ec571de7
Alexandra Liubetskaya <a.li.00@mail.ru>
2018-09-04 01:39:01 +0300

    about and location info, styles

--- main.js
+++ main.js
@@ -1,28 +1,38 @@
 // Get the modal
-/*var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+    var locationModal = document.getElementById('locationModal');
+    var aboutModal = document.getElementById('aboutModal');
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    // Get the button that opens the modal
+    var locationBtn = document.getElementById('locationBtn');
+    var aboutBtn = document.getElementById('aboutBtn');
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+    // When the user clicks the button, open the modal 
+    locationBtn.onclick = function() {
+        locationModal.style.display = "block";
+    }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+    aboutBtn.onclick = function() {
+        aboutModal.style.display = "block";
+    }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
+    // When the user clicks on <span> (x), close the modal
+    function closeModal(modalId) {
+        var modal = document.getElementById(modalId);
         modal.style.display = "none";
     }
-}*/
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == locationModal){
+            locationModal.style.display = "none";
+        }
+        if (event.target == aboutModal){
+            aboutModal.style.display = "none";
+        }
+    }
+
+
 
 function showGrop(){
     document.getElementById('indivform').style.display ='none';
